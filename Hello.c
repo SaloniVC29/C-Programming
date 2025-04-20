b5afedd405d79da59e3da09e4a2d4ecbd0a25054
SaloniVC29 <salonichukekar@gmail.com>
2025-04-20 12:14:32 +0530

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   //including 'stdio.h' file for the usability of print function and etc
+
+int main ()   //Declaration of main function
+{
+  printf("Hello World...!!");    //print function
+
+  return 0;
+}
+
