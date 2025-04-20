0ca23257782a0c94f16b5f776ef0f87e00b685a9
SaloniVC29 <salonichukekar@gmail.com>
2025-04-20 12:48:38 +0530

    Create Area1.c
    Area of Sqaure: Accepts the value of side from user and returns the area of square.

---
+++ Area1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int side;
+ 
+ 
+  printf("Enter the side of sqaure :\n");
+  scanf("%d", &side);
+  
+   int area = side * side;
+
+  printf("The Area of square is : %d", area);
+
+ return 0; 
+}
