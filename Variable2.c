2223d52baa4dcef38f38a43ae293705f54747f8c
SaloniVC29 <salonichukekar@gmail.com>
2025-04-20 12:27:52 +0530

    Create Variable2.c
    It contains the declaration of variables and the print them.

---
+++ Variable2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int age = 22;
+  int price = 500;
+  float pi = 3.14;
+  char percentage = '%';
+
+  printf("Age is :%d/n",age);
+  printf("Pricege is :%d/n",picee);
+  printf("Value of PI is :%f/n",pi);
+  printf("Percentage is :%c/n",percentage);
+
+  return 0;
+}
