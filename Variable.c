1675e4612d658232ea717b003917b218c279f846
SaloniVC29 <salonichukekar@gmail.com>
2025-04-20 12:21:04 +0530

    Create Variable.c
    Types of variables, their definition and declaration.

---
+++ Variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int number;    //integer type variable
+  int age;    //integer type variable
+  int price = 500;   //integer type variable with value assigned
+  float pi = 3.14;   //float type variable
+  char percentage =  '%';   //character type variable
+
+return 0;
+}
